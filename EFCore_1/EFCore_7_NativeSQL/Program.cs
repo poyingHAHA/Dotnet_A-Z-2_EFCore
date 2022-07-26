ffa0acb28677774dde39fc2af376f91363090dd0
poyingHAHA <poying.code@gmail.com>
2022-07-27 07:40:46 +0800

    EFCore14 - 使用ADO.NET與Dapper進行複雜的原生SQL查詢

--- EFCore_1/EFCore_7_NativeSQL/Program.cs
+++ EFCore_1/EFCore_7_NativeSQL/Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -15,12 +16,41 @@ namespace EFCore_4_Relation
             
             using(MyDbContext ctx = new MyDbContext())
             {
-                FormattableString fstr = @$"insert into T_Articles(Title, Message)
-                     select Title, Message from T_Articles where id>{id}";
-                // 加@可以寫多行的字符串
-                Console.WriteLine(fstr.ArgumentCount);
-                Console.WriteLine(String.Join(", ", fstr.GetArguments());
-                await ctx.Database.ExecuteSqlInterpolatedAsync(fstr);
+                //FormattableString fstr = @$"insert into T_Articles(Title, Message)
+                //     select Title, Message from T_Articles where id>{id}";
+                //// 加@可以寫多行的字符串
+                //Console.WriteLine(fstr.ArgumentCount);
+                //Console.WriteLine(String.Join(", ", fstr.GetArguments());
+                //await ctx.Database.ExecuteSqlInterpolatedAsync(fstr);
+
+                //    string titlePattern = "%台%";
+                //    var querable = ctx.Articles.FromSqlInterpolated($@"select * from T_Articles 
+                //                    where Title like {titlePattern} order by newid()");
+
+                //    foreach(var a in querable)
+                //    {
+                //        Console.WriteLine(a.Id+", "+a.Title);
+                //    }
+
+                DbConnection conn = ctx.Database.GetDbConnection(); // 獲得底層Connection對象
+                if(conn.State != System.Data.ConnectionState.Open)
+                {
+                    await conn.OpenAsync();
+                }
+
+                using(var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "select Title, Count(*) from T_Articles group by Title";
+                    using(var reader = await cmd.ExecuteReaderAsync())
+                    {
+                        while (await reader.ReadAsync())
+                        {
+                            string title = reader.GetString(0);
+                            int count = reader.GetInt32(1);
+                            Console.WriteLine($"{title}:{count}");
+                        }
+                    }
+                }
             }
         }
     }
